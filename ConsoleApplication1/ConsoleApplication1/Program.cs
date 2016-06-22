f801535ebfca76effea181d9ae98daf64166c6ca
izzitech <ivan@izzi.com.ar>
2016-06-22 15:56:06 -0300

    New Project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            if (ran.Next(0, 100) < 50)
+            {
+                Console.WriteLine("Mate");
+            }
+            else
+            {
+                Console.WriteLine("Cafe");
+            }
+
+            while (true) ;
+        }
+    }
+}
